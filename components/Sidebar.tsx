7966bb79fad67f282de86bec97d6d1732a7f8be5
sudongyu <976499226@qq.com>
2022-04-26 23:20:36 +0800

    chore: update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,45 @@
 import React from 'react'
-
+import {
+  HomeIcon,
+  SearchIcon,
+  LibraryIcon,
+  PlusCircleIcon,
+  HeartIcon,
+  RssIcon,
+} from "@heroicons/react/outline";
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+      <div className="space-y-4">
+      <button className="flex items-center space-x-2 hover:text-white">
+        <HomeIcon className="h-5 w-5"/>
+        <p>Home</p>
+      </button>
+      <button className="flex items-center space-x-2 hover:text-white">
+        <SearchIcon className="h-5 w-5"/>
+        <p>Search</p>
+      </button>
+      <button className="flex items-center space-x-2 hover:text-white">
+        <LibraryIcon className="h-5 w-5"/>
+        <p>Your Library</p>
+      </button>
+      <hr className="border-t-[0.1px] border-gary-900" />
+      <button className="flex items-center space-x-2 hover:text-white">
+        <HeartIcon className="h-5 w-5"/>
+        <p>Create Playlist</p>
+      </button>
+      <button className="flex items-center space-x-2 hover:text-white">
+        <RssIcon className="h-5 w-5"/>
+        <p>Liked Songs</p>
+      </button>
+      <button className="flex items-center space-x-2 hover:text-white">
+        <LibraryIcon className="h-5 w-5"/>
+        <p>Your Episodes</p>
+      </button>
+      <hr className="border-t-[0.1px] border-gary-900" />
+      </div>
+      {/* PlayLists */}
+    </div>
   )
 }
 
