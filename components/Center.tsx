ba580a1ce872e1af9b4e2bb92eae0073fdcf43c9
sudongyu <976499226@qq.com>
2022-04-28 23:59:30 +0800

    chore: update avator

--- components/Center.tsx
+++ components/Center.tsx
@@ -7,15 +7,20 @@ function Center() {
   console.log(session);
   return (
     <div className="flex-grow flex">
-      <header>
-        <div className="flex items-center space-x-3 bg-black opacity-90 hover:opacity-80 
-        cursor-pointer rounded-full
-        p-1 pr-2">
+      <header className="absolute top-5 right-8">
+        <div className="flex items-center space-x-3 opacity-90 hover:opacity-80 
+        cursor-pointer rounded-full bg-red-300
+        p-1 pr-2 ">
           <img className="rounded-full w-10 h-10" src={session?.user?.image!} alt="" />
           <h2>{session?.user?.name}</h2>
           <ChevronDownIcon className="w-5 h-5"/>
         </div>
       </header>
+
+      <section className={`flex items-end space-x-7 bg-gradient-to-b to-black from-red-500 h-80 text-white p-8`}>
+        {/* <img src="" alt="" /> */}
+        <h1>hello</h1>
+      </section>
     </div>
   )
 }
