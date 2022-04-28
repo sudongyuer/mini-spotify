fe4a4a8a38fe982462db76585588f2faf5778f44
sudongyu <976499226@qq.com>
2022-04-28 13:55:40 +0800

    chore: update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,8 +7,11 @@ import {
   HeartIcon,
   RssIcon,
 } from "@heroicons/react/outline";
-import { signOut } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 function Sidebar() {
+  const {data:session,status}= useSession()
+
+  console.log(session);
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
       <div className="space-y-4">
