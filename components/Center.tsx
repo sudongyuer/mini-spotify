62bf86e4d7e6bf32e1e5b50b98a7a9ddc33cfb8c
sudongyu <976499226@qq.com>
2022-04-28 22:43:16 +0800

    chore: update Center.tsx

--- components/Center.tsx
+++ components/Center.tsx
@@ -1,9 +1,17 @@
+import { useSession } from 'next-auth/react'
 import React from 'react'
 
 function Center() {
+  const {data:session} = useSession()
+  console.log(session);
   return (
     <div className="flex-grow">
-      <h1>Center</h1>
+      <h1>I am the Center</h1>
+      <header>
+        <div>
+          <img src={session?.user?.image} alt="" />
+        </div>
+      </header>
     </div>
   )
 }
