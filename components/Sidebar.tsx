44797ad246964a0c66c7187c49c351846883606a
sudongyu <976499226@qq.com>
2022-04-28 22:10:48 +0800

    chore: update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,7 +13,7 @@ function Sidebar() {
 
   console.log(session);
   return (
-    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll h-screen scrollbar-hide">
       <div className="space-y-4">
         <button className="flex items-center space-x-2 hover:text-white"
           onClick={() => {
@@ -22,6 +22,7 @@ function Sidebar() {
         >
           <p>Logout</p>
         </button>
+
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
           <p>Home</p>
@@ -35,19 +36,21 @@ function Sidebar() {
           <p>Your Library</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900" />
+
         <button className="flex items-center space-x-2 hover:text-white">
-          <HeartIcon className="h-5 w-5" />
+          <PlusCircleIcon className="h-5 w-5" />
           <p>Create Playlist</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <RssIcon className="h-5 w-5" />
+          <HeartIcon className="h-5 w-5" />
           <p>Liked Songs</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <LibraryIcon className="h-5 w-5" />
+          <RssIcon className="h-5 w-5" />
           <p>Your Episodes</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900" />
+
       </div>
       {/* PlayLists */}
       <div className="space-y-4">
