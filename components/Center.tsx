061e8db10ec11b706f1cd4602c0353a1cecf6b67
sudongyu <976499226@qq.com>
2022-04-28 23:35:43 +0800

    chore: update Center.tsx

--- components/Center.tsx
+++ components/Center.tsx
@@ -1,3 +1,4 @@
+import { ChevronDownIcon } from '@heroicons/react/outline';
 import { useSession } from 'next-auth/react'
 import React from 'react'
 
@@ -5,11 +6,14 @@ function Center() {
   const {data:session} = useSession()
   console.log(session);
   return (
-    <div className="flex-grow">
-      <h1>I am the Center</h1>
+    <div className="flex-grow flex">
       <header>
-        <div>
-          <img src={session?.user?.image} alt="" />
+        <div className="flex items-center space-x-3 bg-black opacity-90 hover:opacity-80 
+        cursor-pointer rounded-full
+        p-1 pr-2">
+          <img className="rounded-full w-10 h-10" src={session?.user?.image!} alt="" />
+          <h2>{session?.user?.name}</h2>
+          <ChevronDownIcon className="w-5 h-5"/>
         </div>
       </header>
     </div>
