c18a95b17065ca8d7602a4d70ccd1ffbc460c990
sudongyu <976499226@qq.com>
2022-04-29 00:35:11 +0800

    chore: update Sudebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   HomeIcon,
   SearchIcon,
@@ -8,10 +8,23 @@ import {
   RssIcon,
 } from "@heroicons/react/outline";
 import { signOut, useSession } from 'next-auth/react';
+import useSpotify from '../hooks/useSpotify';
 function Sidebar() {
+  const spotifyApi = useSpotify()
   const {data:session,status}= useSession()
+  const [playlists,setPlaylists] = useState<SpotifyApi.PlaylistObjectSimplified[]>([])
 
-  console.log(session);
+  useEffect(()=>{
+
+    if(spotifyApi.getAccessToken()){
+      spotifyApi.getUserPlaylists().then(data=>{
+        setPlaylists(data.body.items)
+      })
+    }
+  },[session,spotifyApi])
+
+  console.log(playlists);
+
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll h-screen scrollbar-hide">
       <div className="space-y-4">
