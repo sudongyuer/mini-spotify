3246eff3cc4e2e82b7fb199867c256e542486333
sudongyu <976499226@qq.com>
2022-04-29 14:16:09 +0800

    chore: update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import { RecoilRoot } from 'recoil'
 
 function MyApp({ Component, pageProps : {session,...pageProps}}: AppProps) {
   return (
     <SessionProvider session={session}>
+      <RecoilRoot>
       <Component {...pageProps} />
+      </RecoilRoot>
     </SessionProvider>
   )
 }
