2b021a3c4f12d7a0979b3853ba8a4ce730100ad4
sudongyu <976499226@qq.com>
2022-04-30 23:05:45 +0800

    chore: update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -29,7 +29,8 @@ function Sidebar() {
   console.log(playlists);
 
   return (
-    <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll h-screen scrollbar-hide">
+    <div className="text-gray-500 p-5 text-xs sm:max-w-[12rem] lg:max-w-[15rem]  
+     lg:text-sm border-r border-gray-900 overflow-y-scroll h-screen scrollbar-hide hidden md:block">
       <div className="space-y-4">
         <button className="flex items-center space-x-2 hover:text-white"
           onClick={() => {
