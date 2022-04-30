e802356f2f0293c0f6c71072db030a5280b9f3a7
sudongyu <976499226@qq.com>
2022-04-30 15:54:11 +0800

    feat: add playlistState

--- atoms/playlistAtom.ts
+++ atoms/playlistAtom.ts
@@ -1,5 +1,10 @@
 import { atom } from "recoil";
 
+export const playlistState=atom<SpotifyApi.SinglePlaylistResponse|null>({
+  key:"playlistState",
+  default:null
+})
+
 export const playlistIdState = atom({
   key:"playlistIdState",
   default:'5xuHVnh4ZQRTag5gL4nyBJ',
