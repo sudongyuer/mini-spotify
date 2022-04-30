655bb68269cae4bf9ba61422e005aba3ca97c6b9
sudongyu <976499226@qq.com>
2022-04-30 18:46:13 +0800

    chore: update useSpotify.ts

--- hooks/useSpotify.ts
+++ hooks/useSpotify.ts
@@ -1,13 +1,14 @@
 import { signIn ,useSession } from 'next-auth/react';
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import SpotifyWebApi from 'spotify-web-api-node';
 
-const spotifyApi = new SpotifyWebApi({
+const rawSpotifyApi = new SpotifyWebApi({
   clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
   clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
   redirectUri: process.env.REDIRECT_URI,
 });
 function useSpotify() {
+  const [spotifyApi,setSpotifyApi]=useState(rawSpotifyApi)
   const {data:session,status} = useSession()
   useEffect(()=>{
     if(session){
@@ -17,6 +18,8 @@ function useSpotify() {
       } 
       //@ts-ignore
       spotifyApi.setAccessToken(session.user.accessToken)
+      const newSpotifyApi = Object.create(spotifyApi)
+      setSpotifyApi(newSpotifyApi)
     }
   },[session])
   return spotifyApi
