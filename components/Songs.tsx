0181a974eddaabb5a82b0a9382665e746ac9a1d5
sudongyu <976499226@qq.com>
2022-05-02 18:40:48 +0800

    chore: update Songs.tsx

--- components/Songs.tsx
+++ components/Songs.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { useRecoilValue } from 'recoil'
 import { playlistState } from '../atoms/playlistAtom'
+import Song from './Song';
 
 function Songs() {
   const playlist = useRecoilValue(playlistState)
   console.log(playlist);
   return (
     <div className="text-white">
-      {playlist?.tracks.items.map(track => (
-      <div key={track.track?.id}>{track.track?.name}</div>
+      {playlist?.tracks.items.map((track,index) => (
+        <Song key={track.track?.id} track={track} order={index}/>
+      // <div key={track.track?.id}>{track.track?.name}</div>
       ))}
     </div>
   )
