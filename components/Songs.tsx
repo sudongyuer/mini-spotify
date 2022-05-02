3ddd132d61ac2a8dcafef46b29b9dab1048cc771
sudongyu <976499226@qq.com>
2022-05-02 22:35:26 +0800

    chore: update Songs.tsx

--- components/Songs.tsx
+++ components/Songs.tsx
@@ -7,7 +7,7 @@ function Songs() {
   const playlist = useRecoilValue(playlistState)
   console.log(playlist);
   return (
-    <div className="text-white">
+    <div className="text-white p-8 pt-0">
       {playlist?.tracks.items.map((track,index) => (
         <Song key={track.track?.id} track={track} order={index}/>
       // <div key={track.track?.id}>{track.track?.name}</div>
