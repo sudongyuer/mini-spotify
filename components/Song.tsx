7cb3dd36a85a1682b02fc607a50515fd60e84eeb
sudongyu <976499226@qq.com>
2022-05-04 00:30:03 +0800

    chore: update Songs.tsx

--- components/Song.tsx
+++ components/Song.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import { useRecoilState } from 'recoil'
+import { currentTrackIdState, isPlayingState } from '../atoms/songAtom'
 import useSpotify from '../hooks/useSpotify'
 import { millisToMinutesAndSeconds } from '../lib/time'
 
 function Song({ track, order }: { track: SpotifyApi.PlaylistTrackObject, order: number }) {
   const spotifyApi = useSpotify()
+  const [currentTrackId , setCurrentTrackId] = useRecoilState(currentTrackIdState)
+  const [isPlaying , setIsPlaying] = useRecoilState(isPlayingState)
+  const playSong = ()=>{
+    setCurrentTrackId(track.track?.id)
+    setIsPlaying(true)
+    spotifyApi.play({
+      uris: [track.track?.uri!]
+    })
+  }
   return (
     <div className="grid  grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900
-     rounded-lg cursor-pointer">
+     rounded-lg cursor-pointer" onClick={playSong}>
       <div className='flex items-center space-x-4'>
         <p>{order + 1}</p>
         <img src={track.track?.album.images[0].url} alt="" className='h-10 w-10' />
