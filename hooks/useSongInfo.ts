71162b4f37d5b09e4020ff437e9195323ece08bd
sudongyu <976499226@qq.com>
2022-05-04 22:31:45 +0800

    chore: update useSonginfo.ts

--- hooks/useSongInfo.ts
+++ hooks/useSongInfo.ts
@@ -6,12 +6,26 @@ import useSpotify from './useSpotify'
 function useSongInfo() {
   const spotifyApi = useSpotify()
   const [currentTrackId, setCurrentIdTrack] = useRecoilState(currentTrackIdState)
-  const [songInfo, setSongInfo] = useState(null)
-
+  type song = Awaited<Promise<SpotifyApi.SingleTrackResponse>>
+  const [songInfo, setSongInfo] = useState<null|song>(null)
   useEffect(() => {
-
+    const fetchSongInfo = async () => {
+      if (currentTrackId) {
+        const trackInfo = await spotifyApi.getTrack(currentTrackId)
+        // const trackInfo = await fetch(
+        //   `https://api.spotify.com/v1/tracks/${currentTrackId}`,
+        //   {
+        //     headers: {
+        //       Authorization: "Bearer " + spotifyApi.getAccessToken(),
+        //     },
+        //   }
+        // ).then((res) => res.json());
+        setSongInfo(trackInfo.body)
+      }
+    }
+    fetchSongInfo()
   }, [currentTrackId, spotifyApi])
-  
+
   return songInfo
 }
 
