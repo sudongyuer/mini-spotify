41826419b2f2eba759f3b3f97ad6231585c2d0f3
sudongyu <976499226@qq.com>
2022-05-05 00:24:43 +0800

    chore: update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -30,7 +30,7 @@ function Sidebar() {
 
   return (
     <div className="text-gray-500 p-5 text-xs sm:max-w-[12rem] lg:max-w-[15rem]  
-     lg:text-sm border-r border-gray-900 overflow-y-scroll h-screen scrollbar-hide hidden md:block">
+     lg:text-sm border-r border-gray-900 overflow-y-scroll h-screen scrollbar-hide hidden md:block pb-36">
       <div className="space-y-4">
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
