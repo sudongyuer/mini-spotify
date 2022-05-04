da1cac004fb21245ff9967ed94a846966bf8616b
sudongyu <976499226@qq.com>
2022-05-05 01:58:25 +0800

    chore: update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,7 +6,7 @@ import {
   PlusCircleIcon,
   HeartIcon,
   RssIcon,
-} from "@heroicons/react/outline";
+} from "@heroicons/react/solid";
 import { signOut, useSession } from 'next-auth/react';
 import useSpotify from '../hooks/useSpotify';
 import { useRecoilState } from 'recoil';
@@ -51,11 +51,11 @@ function Sidebar() {
           <p>Create Playlist</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <HeartIcon className="h-5 w-5" />
+          <HeartIcon className="h-5 w-5 text-blue-500" />
           <p>Liked Songs</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <RssIcon className="h-5 w-5" />
+          <RssIcon className="h-5 w-5 text-blue-500" />
           <p>Your Episodes</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900" />
